0ccbf3372edc0258c8accd28387d3f645300f237
RocketRedNeck <the.rocketredneck@gmail.com>
2021-05-12 23:18:27 -0700

    RadioClock next steps

--- RadioTime4/RadioTime4.ino
+++ RadioTime4/RadioTime4.ino
@@ -106,7 +106,7 @@ void setup()
 // The loop routine runs over and over again forever:
 // ----------------------------------------------------------------------------------------------------
 bool on = false;
-unsigned long pw_ms = 0;
+long long pw_ms = 0;
 enum State
 {
   WAITING_FOR_END,
@@ -131,10 +131,23 @@ const unsigned long BIT_1_LOW_MS  = 400;
 const unsigned long BIT_1_HIGH_MS = 600;
 
 bool decodeTime = false;
-int minutes = -1;
-int hours = -1;
-int days = -1;
-int year = -1;
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+int days = 0;
+int year = 0;
+
+MicroTimer pulseTimer;
+enum PulseState
+{
+  WAITING_FOR_RISING,
+  WAITING_FOR_FALLING
+};
+PulseState pulseState = WAITING_FOR_RISING;
+bool newPulse = false;
+
+MicroTimer displayTimer;
+long long nextDisplay_ms = 1000;
 
 void loop()
 {
@@ -144,108 +157,153 @@ void loop()
     // 500 ms dropout = 1
     // 800 ms dropout = mark (end of 10 second interval and first mark at minute)
     // Note: The end/start of minute is two (2) 800 ms marks in a row.
-    // If no pulse within 950 ms, timeout and try again
-    pw_ms = pulseInLong(DP_WWVB_INVERTED, HIGH, 2000000) / 1000;
-
-    switch(state)
+    
+    // The problem with pulseIn or pulseInLong is that they block the
+    // loop() from running until the both edges or timeout occurs.
+    // We we really, really want is to keep polling for the pulse while
+    // doing other things
+    switch(pulseState)
     {
-      case WAITING_FOR_END:
-        if (pw_ms > MARK_LOW_MS)
+      case WAITING_FOR_RISING:
+        if (HIGH == digitalRead(DP_WWVB_INVERTED))
         {
-          state = WAITING_FOR_START;
+          pulseTimer.mark();
+          pulseState = WAITING_FOR_FALLING;
+        }
+        else if (pulseTimer.read_ms() > 2000)
+        {
+          pulseTimer.mark();
+          pw_ms = 0;
+          pulseState = WAITING_FOR_RISING;
+          newPulse = true;
+        }
+        break;
+      case WAITING_FOR_FALLING:
+        if (LOW == digitalRead(DP_WWVB_INVERTED))
+        {
+          pw_ms = pulseTimer.read_ms();
+          pulseState = WAITING_FOR_RISING;
+          newPulse = true;
+        }
+        else if (pulseTimer.read_ms() > 2000)
+        {
+          pulseTimer.mark();
+          pw_ms = 0;
+          pulseState = WAITING_FOR_RISING;
+          newPulse = true;
+        }
+        break;
+      default:
+        pulseState = WAITING_FOR_RISING;
+        break;
+      
+    };
 
-          // Decode time and display
-          decodeTime = true;
-        }
-        break;
-      case WAITING_FOR_START:
-        if (pw_ms > MARK_LOW_MS)
-        {
-          topOfMinute_ms = timer.read_ms() - pw_ms; 
-          nextMinuteWindow_ms = topOfMinute_ms + 60000;
-          nextMarkWindow_ms   = topOfMinute_ms +  9000;
-          nextBitWindow_ms    = topOfMinute_ms +  1000;
-          state = WAITING_FOR_BIT;
-          index = 0;
-          data[index] = 1;
-          ++index;
-        }
-        else
-        {
-          state = WAITING_FOR_END;
-        }
-        break;
-      case WAITING_FOR_BIT:
-        if (timer.read_ms() >= nextBitWindow_ms)
-        {
-          nextBitWindow_ms += 1000;
-          if ( (pw_ms > BIT_1_LOW_MS) && (pw_ms < BIT_1_HIGH_MS) )
+    if (newPulse)
+    {
+      newPulse = false;
+      
+      switch(state)
+      {
+        case WAITING_FOR_END:
+          if (pw_ms > MARK_LOW_MS)
           {
-            // Record 1
-            data[index] = 1;
+            state = WAITING_FOR_START;
+  
+            // Decode time and display
+            ++index;
+            decodeTime = true;
           }
-          else if ( (pw_ms > BIT_0_LOW_MS) && (pw_ms < BIT_0_HIGH_MS))
+          break;
+        case WAITING_FOR_START:
+          if (pw_ms > MARK_LOW_MS)
           {
-            // Record 0
-            data[index] = 0;
+            topOfMinute_ms = timer.read_ms() - pw_ms;
+            nextMinuteWindow_ms = topOfMinute_ms + 60000;
+            nextMarkWindow_ms   = topOfMinute_ms +  9000;
+            nextBitWindow_ms    = topOfMinute_ms +  1000;
+            state = WAITING_FOR_BIT;
+            index = 0;
+            seconds = 0; 
+            data[index] = 1;
+            ++index;
           }
           else
           {
-            // Invalid bit
-            data[index] = -1;
+            state = WAITING_FOR_END;
           }
-          ++index;
-          if (0 == ((index+1)%10))
+          break;
+        case WAITING_FOR_BIT:
+          if (timer.read_ms() >= nextBitWindow_ms)
           {
-            if (59 == index)
+            nextBitWindow_ms += 1000;
+            if ( (pw_ms > BIT_1_LOW_MS) && (pw_ms < BIT_1_HIGH_MS) )
             {
-              state = WAITING_FOR_END;
+              // Record 1
+              data[index] = 1;
+            }
+            else if ( (pw_ms > BIT_0_LOW_MS) && (pw_ms < BIT_0_HIGH_MS))
+            {
+              // Record 0
+              data[index] = 0;
             }
             else
             {
-              state = WAITING_FOR_MARK;
+              // Invalid bit
+              data[index] = -1;
             }
-          }
-//          switch (index)
-//          {
-//            case 9:
-//            case 19:
-//            case 29:
-//            case 39:
-//            case 49:
-//              state = WAITING_FOR_MARK;
-//              break;
-//            case 59:
-//              state = WAITING_FOR_END;
-//              break;
-//            default:
-//              break;
-//          }
-        }
-        break;
-      case WAITING_FOR_MARK:
-        if (timer.read_ms() >= nextMarkWindow_ms)
-        {
-          if (pw_ms > MARK_LOW_MS)
-          {
-            data[index] = 1;
+            ++index;
+            if (0 == ((index+1)%10))
+            {
+              if (59 == index)
+              {
+                state = WAITING_FOR_END;
+              }
+              else
+              {
+                state = WAITING_FOR_MARK;
+              }
+            }
           }
           else
           {
-            // Invalid mark
-            data[index] = -1;
+            // Extraneous pulse received before next pulse window
+            // means previous pulse that was recorded may have
+            // been chopped. Go back and invalidate it.
+            data[index-1] = -1;
           }
-          ++index;
-          nextMarkWindow_ms += 10000;
-          nextBitWindow_ms  +=  1000;
-          state = WAITING_FOR_BIT;
-        }
-        break;
-
-      default:
-        state = WAITING_FOR_END;
-    };
-
+          break;
+        case WAITING_FOR_MARK:
+          if (timer.read_ms() >= nextMarkWindow_ms)
+          {
+            if (pw_ms > MARK_LOW_MS)
+            {
+              data[index] = 1;
+            }
+            else
+            {
+              // Invalid mark
+              data[index] = -1;
+            }
+            ++index;
+            nextMarkWindow_ms += 10000;
+            nextBitWindow_ms  +=  1000;
+            state = WAITING_FOR_BIT;
+          }
+          else
+          {
+            // Extraneous pulse received before next pulse window
+            // means previous pulse that was recorded may have
+            // been chopped. Go back and invalidate it.
+            data[index-1] = -1;          
+          }
+          break;
+  
+        default:
+          state = WAITING_FOR_END;
+      };
+    } // end if newPulse
+    
     if (decodeTime)
     {
       if (data[1]>=0 &&
@@ -279,31 +337,95 @@ void loop()
         hours += (data[16]?4:0);
         hours += (data[17]?2:0);
         hours += (data[18]?1:0);
+
+        days = 0;
+        days += (data[22]?200:0);
+        days += (data[23]?100:0);
+        days += (data[25]?80:0);
+        days += (data[26]?40:0);
+        days += (data[27]?20:0);
+        days += (data[28]?10:0);
+        days += (data[30]?8:0);
+        days += (data[31]?4:0);
+        days += (data[32]?2:0);
+        days += (data[33]?1:0);
+
+        year = 0;
+        year += (data[45]?80:0);
+        year += (data[46]?40:0);
+        year += (data[47]?20:0);
+        year += (data[48]?10:0);
+        year += (data[50]?8:0);
+        year += (data[51]?4:0);
+        year += (data[52]?2:0);
+        year += (data[53]?1:0);
+
+        if (hours != 0 && minutes != 0 && days != 0 && year != 0)
+        {
+          hours -= 7; // Tucson offset
+          if (hours < 0)
+          {
+            hours += 24;
+            days -= 1;
+          }
+        }      
       }
       else
       {
-        hours = -1;
-        minutes = -1;
+        hours = 0;
+        minutes = 0;
+        days = 0;
+        year = 0;
       }
             
       decodeTime = false;
     }
 
     // Display output (either LCD or Nixie)
-    lcd.clear();
-    lcd.home();
-    lcd.print("PW = "); 
-    lcd.print(pw_ms);
-    lcd.print(" B = ");
-    lcd.print(data[(index == 0)?0:(index-1) % 60]);
-    lcd.setCursor(0,1);
-    lcd.print("S=");
-    lcd.print(state);
-    lcd.print(" ");
-    lcd.print(hours);
-    lcd.print(":");
-    lcd.print(minutes);
-    lcd.print(":");
-    lcd.print(index-1);      
-   
+    if (displayTimer.read_ms() >= nextDisplay_ms)
+    {
+      nextDisplay_ms += 1000;
+      
+      lcd.clear();
+      lcd.home();
+      lcd.print("PW = "); 
+      lcd.print((long)pw_ms);
+      lcd.print(" D = ");
+      if (days < 100)
+      {
+        lcd.print(0);
+      }
+      if (days < 10)
+      {
+        lcd.print(0);
+      }
+      lcd.print(days);
+      lcd.setCursor(0,1);
+      lcd.print("S=");
+      lcd.print(state);
+      lcd.print(" ");
+      if (hours < 10)
+      {
+        lcd.print(0);
+      }
+      lcd.print(hours);
+      lcd.print(":");
+      if (minutes < 10)
+      {
+        lcd.print(0);
+      }
+      lcd.print(minutes);
+      lcd.print(":");
+      if (seconds < 10)
+      {
+        lcd.print(0);
+      }
+      lcd.print(seconds++);
+      lcd.print(" ");
+      if (year < 10)
+      {
+        lcd.print(0);
+      }
+      lcd.print(year);
+    }   
 }
